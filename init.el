04e0d4bc743e50342ae644201765a1b9f6c5ff7a
Matt Cavalier <matthewjcavalier@pm.me>
2021-02-20 15:06:02 -0600

    added themeing

--- init.el
+++ init.el
@@ -9,7 +9,6 @@
 (setq visible-bell t)
 
 (set-face-attribute 'default nil :font "JetBrains Mono NL Medium" :height 120)
-(load-theme 'tango-dark)
 
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -34,7 +33,13 @@
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 (use-package counsel
-  :ensure t)
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil));; Don't start searches with ^
 
 (use-package ivy
   :diminish ;; hide from mode line
@@ -54,17 +59,65 @@
   :config
   (ivy-mode 1))	 
 
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+  ;; :ensure t
+  ;; :config)
+
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
 
+;;ui
+(column-number-mode)
+(global-display-line-numbers-mode t)
+;;Disable line num for some mode
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda() (display-line-numbers-mode 0))))
 
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package afternoon-theme
+  :ensure t)
+(use-package flatland-theme
+  :ensure t)
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package doom-themes)
+
+
+(load-theme 'doom-one)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-modeline counsel consel ivy evil use-package)))
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(custom-safe-themes
+   '("8f5a7a9a3c510ef9cbb88e600c0b4c53cdcdb502cfe3eb50040b7e13c6f4e78e" "730a87ed3dc2bf318f3ea3626ce21fb054cd3a1471dcd59c81a4071df02cb601" "7b3d184d2955990e4df1162aeff6bfb4e1c3e822368f0359e15e2974235d9fa8" "0a41da554c41c9169bdaba5745468608706c9046231bbbc0d155af1a12f32271" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "fd22c8c803f2dac71db953b93df6560b6b058cb931ac12f688def67f08c10640" "57e3f215bef8784157991c4957965aa31bac935aca011b29d7d8e113a652b693" "eb122e1df607ee9364c2dfb118ae4715a49f1a9e070b9d2eb033f1cefd50a908" default))
+ '(package-selected-packages
+   '(doom-themes helpful ivy-rich which-key flatland-theme afternoon-theme rainbow-delimiters doom-modeline counsel consel ivy evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
